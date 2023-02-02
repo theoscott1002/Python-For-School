dcf7fd83c8841e1b1210d5750e1cf373f0c91a05
ayoadejoe <ariyotheophilus@gmail.com>
2023-02-02 17:07:52 +0100

    Worked on loops and calculator

--- practice.py
+++ practice.py
@@ -1,2 +1,17 @@
-print("Hello Theo")
-print("I am a big boy")
+print("WELCOME!!!")
+Num1 = float(input("Please enter first number:"))
+Num2 = float(input("Please enter second number:"))
+operation = input("Please enter operation:")
+if operation == "add":
+    Sum = Num1 + Num2
+    print("The answer is:" + str(Sum))
+elif operation == "subtract":
+    diff = Num1-Num2
+    print("The answer is:" + str(diff))
+elif operation == "multiply":
+    prod = Num1*Num2
+    print("The answer is:" + str(prod))
+elif operation == "divide":
+    div = Num1/Num2
+    print("The answer is:" + str(div))
+else: print("INVALID OPERATION!!!")
