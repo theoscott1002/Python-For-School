0404c22bf3d8d16c6d87440a9be0a5e540ec5075
ayoadejoe <ariyotheophilus@gmail.com>
2023-02-07 20:59:44 +0100

    created a linked list

---
+++ practice3.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self, value):
+        newNode = Node(value)
+        self.head = newNode
+        self.tail = newNode
+        self.length = 1
+
+    def insert_at_end(self, data):
+        newNode = Node(data)
+
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            self.tail = newNode
+            self.length += 1
+
+        return self
+
+
+def main():
+    myList = LinkedList(5)
+    myList.insert_at_end(10)
+    myList.insert_at_end(15)
+
+    print('here', myList.head.next.next.data)
+
+
+main()
+
+
+
+
