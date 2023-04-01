83b37c290315f40ae394ceff10e134388b186a5c
Theophilus <ariyotheophilus@gmail.com>
2023-04-01 15:07:12 +0100

    Adding element at beginning of a linkedlist

--- practice4.py
+++ practice4.py
@@ -4,38 +4,31 @@ class Node:
         self.next = next
 
 
-class LinkedList:
+class Linkedlist:
     def __init__(self, value):
         newNode = Node(value)
         self.head = newNode
         self.tail = newNode
         self.length = 1
 
-    def insert_at_end(self, data):
+    def insert_at_begin(self, data):
         newNode = Node(data)
-
         if self.head is None:
             self.head = newNode
             self.tail = newNode
         else:
-            self.tail.next = newNode
-            self.tail = newNode
+            self.head.next = self.head
+            self.head = newNode
             self.length += 1
-
         return self
 
 
 def main():
-    myList = LinkedList(5)
-    myList.insert_at_end(10)
-    myList.insert_at_end(15)
-    myList.insert_at_end(20)
-
-    print('here', myList.head.next.next.next.data)
+    myList = Linkedlist(12)
+    myList.insert_at_begin(22)
+    myList.insert_at_begin(30)
+    print("here", myList.head.data)
+    print(myList.tail.data)
 
 
 main()
-
-
-
-
